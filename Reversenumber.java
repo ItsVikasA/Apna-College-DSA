019677c4dc8bbf20fb0423f624a9995aab4e5f2e
Vikas <vikas.ambalazari@gmail.com>
2025-01-28 17:21:45 +0530

    Reversenumber.java

---
+++ Reversenumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Reversenumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rev=0;
+        System.out.println("Enter the number: ");
+        int n =sc.nextInt();
+        while(n>0)
+        {
+            int lastdigit=n%10;
+            rev=(rev*10) + lastdigit;
+            n=n/10;
+        }
+        System.out.println(rev);
+    }
+    
+}
