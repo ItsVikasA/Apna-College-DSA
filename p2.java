df4fb46c20d6f92cf4dd7d8b305edd35b3247f30
Vikas <vikas.ambalazari@gmail.com>
2025-02-03 23:53:57 +0530

    p2.java

---
+++ p2.java
@@ -0,0 +1,13 @@
+public class p2
+{
+    public static void main(String[] args) {
+        for (int l=1;l<=4;l++)
+        {
+            for(int star=1;star<=4-l+1;star++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }    
+}
