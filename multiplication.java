38c2e5cfb53462bb177508b266ada5170fccce06
Vikas <vikas.ambalazari@gmail.com>
2025-02-03 23:09:22 +0530

    multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class multiplication
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int number=sc.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(number + "*" + i + "=" + (number*i));
+        }   
+    }
+}
