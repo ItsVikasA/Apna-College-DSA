5deadbb81c0d3f884a8906b54941292ba38d8abd
Vikas <vikas.ambalazari@gmail.com>
2025-02-05 22:05:31 +0530

    ButterflyPattern.java

---
+++ ButterflyPattern.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class ButterflyPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        // Upper half of the butterfly
+        for (int i = 1; i <= n; i++) {
+            // Left half stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Spaces in the middle
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // Right half stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half of the butterfly
+        for (int i = n; i >= 1; i--) {
+            // Left half stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Spaces in the middle
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // Right half stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
