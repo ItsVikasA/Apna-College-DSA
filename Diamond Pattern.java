c5037bdc48168e41e775b38222ad5f4a651320ea
Vikas <vikas.ambalazari@gmail.com>
2025-02-10 21:51:24 +0530

    DiamondPattern.java

---
+++ Diamond Pattern.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class DiamondPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows for the upper half: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        // Upper half of the diamond
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
