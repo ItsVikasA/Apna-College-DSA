fd06da9e1dfada4e5fa36a4afa926621bbec399c
Vikas <vikas.ambalazari@gmail.com>
2025-02-13 19:00:36 +0530

    RockPaperScissors.java

---
+++ RockPaperScissors.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] choices = {"Rock", "Paper", "Scissors"};
+        
+        System.out.println("Welcome to Rock, Paper, Scissors!");
+        System.out.println("Enter your choice (Rock, Paper, Scissors):");
+
+        while (true) {
+            System.out.print("Your move: ");
+            String userChoice = scanner.next().toLowerCase();
+
+            if (!userChoice.equals("rock") && !userChoice.equals("paper") && !userChoice.equals("scissors")) {
+                System.out.println("Invalid input. Please enter Rock, Paper, or Scissors.");
+                continue;
+            }
+
+            int computerIndex = random.nextInt(3);
+            String computerChoice = choices[computerIndex];
+
+            System.out.println("Computer chose: " + computerChoice);
+
+            // Determine the winner
+            if (userChoice.equals(computerChoice.toLowerCase())) {
+                System.out.println("It's a tie!");
+            } else if ((userChoice.equals("rock") && computerChoice.equals("Scissors")) ||
+                       (userChoice.equals("paper") && computerChoice.equals("Rock")) ||
+                       (userChoice.equals("scissors") && computerChoice.equals("Paper"))) {
+                System.out.println("You win!");
+            } else {
+                System.out.println("You lose!");
+            }
+
+            // Ask if the user wants to play again
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgain = scanner.next().toLowerCase();
+            if (!playAgain.equals("yes")) {
+                System.out.println("Thanks for playing! Goodbye.");
+                break;
+            }
+        }
+        scanner.close();
+    }
+}
