9ba76e4e31f0ab822f95ee9145eb2691377bb3d3
Vikas <vikas.ambalazari@gmail.com>
2025-02-17 08:02:07 +0530

    FloydsTriangle.java

--- FloydsTriangle.java
+++ FloydsTriangle.java
@@ -1,9 +1,7 @@
 public class FloydsTriangle {
     public static void main(String[] args) {
         int rows = 5; // You can change the number of rows as per your requirement
-        int number = 1; // Starts from 1
-
-        // Outer loop for number of rows
+        int number = 1;
         for (int i = 1; i <= rows; i++) {
             // Inner loop to print numbers in each row
             for (int j = 1; j <= i; j++) {
