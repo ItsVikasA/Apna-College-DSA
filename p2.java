5e68961d799e1402da3ada2514a9f5b2d8fff86c
Vikas <vikas.ambalazari@gmail.com>
2025-02-19 07:22:43 +0530

    p2.java

--- p2.java
+++ p2.java
@@ -1,6 +1,6 @@
 public class p2
 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         for (int l=1;l<=4;l++)
         {
             for(int star=1;star<=4-l+1;star++)
