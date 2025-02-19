872a3081cac5730f586e50e965b19193e83f912c
Vikas <vikas.ambalazari@gmail.com>
2025-02-19 07:19:17 +0530

    BottomLeftTriangle.java

--- BottomLeftTriangle.java
+++ BottomLeftTriangle.java
@@ -7,7 +7,7 @@ public class BottomLeftTriangle{
         int n = scanner.nextInt();
 
         for (int i = 1; i <= n; i++){
-            for (int j = 1; j <= i; j++) {
+            for (int j = 1; j <= i; j++){
                 System.out.print("*");
             }
             System.out.println();
