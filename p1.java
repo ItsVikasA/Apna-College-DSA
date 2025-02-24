aff05a134200bb90dcd25297518fb7d5c078a267
Vikas <vikas.ambalazari@gmail.com>
2025-02-24 20:30:01 +0530

    p1.java

--- p1.java
+++ p1.java
@@ -1,8 +1,7 @@
 public class p1
 {
     public static void main(String[] args){
-        for (int l=1;l<=4;l++)
-        {
+        for (int l=1;l<=4;l++){
             for(int star=1;star<=l;star++)
             {
                 System.out.print("*");
