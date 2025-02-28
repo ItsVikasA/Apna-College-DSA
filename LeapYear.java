5adbbf0e571c4d2b8fd908a384fac62ab833c8d9
Vikas <vikas.ambalazari@gmail.com>
2025-02-28 07:32:22 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Leapyear    {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
         System.out.println("Enter the year: ");
         int year = sc.nextInt();
 
