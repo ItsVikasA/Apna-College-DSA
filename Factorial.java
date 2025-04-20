8560c2e2113ba2fcf9d7c45526b21d8362701ced
Vikas <vikas.ambalazari@gmail.com>
2025-04-20 16:40:06 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Factorial      {
-    public static void main(String[] args)
+ public static void main(String[] args)
      {
         int fact=1;
         int number;
