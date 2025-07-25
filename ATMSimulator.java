14133e3fcf4359cc95662ccf3bf6ae8ef6d2176d
Vikas <vikas.ambalazari@gmail.com>
2025-07-25 09:38:32 +0530

    ATMSimulator.java

--- ATMSimulator.java
+++ ATMSimulator.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;                
 class ATM {                               
 private double balance;  
-
 public ATM(double initialBalance) {     
         this.balance = initialBalance;
     }
