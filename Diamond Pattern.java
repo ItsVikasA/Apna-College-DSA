d45939c90f72abd9ba0c986474b68f9efeb5f61d
Vikas <vikas.ambalazari@gmail.com>
2025-08-10 06:47:16 +0530

    Update Diamond Pattern.java

--- Diamond Pattern.java
+++ Diamond Pattern.java
@@ -10,7 +10,7 @@ public class DiamondPattern  {
         for (int i = 1; i <= n; i++){
             // Print spaces
             for (int j = 1; j <= n - i; j++) {
-                System.out.print(" "); 
+                 System.out.print(" "); 
             } 
             // Print stars
             for (int j = 1; j <= (2 * i - 1); j++) {
