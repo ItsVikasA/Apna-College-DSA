6c7f36958df94922100b95bf77cbb4ca58936517
Vikas <vikas.ambalazari@gmail.com>
2025-08-15 09:52:17 +0530

    Update GuessTheNumber.java

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;   
 import java.util.Random; 
-public class GuessTheNumber.  {
+public class GuessTheNumber {
     public static void main(String[] args)   {  
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
