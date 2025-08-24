92347355707dfe9c36569cd2eadde257aca9983c
Vikas <vikas.ambalazari@gmail.com>
2025-08-24 08:26:02 +0530

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Contact  {                  
     String name, phone;  
 
-    Contact(String name, String phone){
+    Contact(String name,String phone){
         this.name = name;
         this.phone = phone;
     }
