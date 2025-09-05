3094dfe1a7abb4c6526870d729263fedd5ea00ba
Vikas <vikas.ambalazari@gmail.com>
2025-09-05 07:07:47 +0530

    Update ButterflyPattern.java

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;               
 public class ButterflyPattern{  
      public static void main(String[] args) {  
-        Scanner sc =newScanner(System.in);
+       Scanner sc =newScanner(System.in);
         System.out.print("Enter the number of rows: ");
         int n = sc.nextInt();  
         sc.close();
