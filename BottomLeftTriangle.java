e69c617247a3eccc2a6f620dea76c326f0ae49eb
Vikas <vikas.ambalazari@gmail.com>
2025-09-10 09:22:05 +0530

    Update BottomLeftTriangle.java

--- BottomLeftTriangle.java
+++ BottomLeftTriangle.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;             
 public class Bottom left triangles {    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); 
         System.out.print("Enter the number of rows: ");
         int n = scanner.nextInt();
         for (int i = 1; i <= n; i++){
