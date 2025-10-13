b415311ae12ef8d57c9a021d229bbd39a19c4dbf
Vikas <vikas.ambalazari@gmail.com>
2025-10-13 15:48:58 +0530

    Update Diamond Pattern.java

--- Diamond Pattern.java
+++ Diamond Pattern.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;                   
-public class DiamondPattern {                 
+public class DiamondPattern {                  
     public static void main(String[] args)  {   
        Scanner scanner = new  Scanner(System.in);
         System.out.print("Enter the number of rows for the upper half: ");
